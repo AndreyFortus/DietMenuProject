3aee533994dff62029b276379bd18ceb2dfeb808
Andrey <andrey020904@gmail.com>
2025-10-08 18:15:19 +0300

    Create core app and add initial tests

---
+++ backend/core/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+def test_example_addition():
+    """Простий тест, щоб перевірити, що все працює."""
+    assert 1 + 1 == 2
+
+def test_truthiness():
+    """Ще один простий тест."""
+    assert True
