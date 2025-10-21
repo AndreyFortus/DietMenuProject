25e0f9616f1d5fafacebcbde97e6e42743eba649
Andrey <andrey020904@gmail.com>
2025-10-21 22:06:20 +0300

    Add Product model with sample data

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    calories_per_100g = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name} ({self.calories_per_100g} kcal/100g)'
