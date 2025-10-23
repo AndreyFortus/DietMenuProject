a6ee8db50975dd283c8461c124f59eb98ea057e9
Andrey <andrey020904@gmail.com>
2025-10-23 17:21:27 +0300

    Add 'category' field to Product model

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,8 +2,25 @@ from django.db import models
 
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('meat', 'Meat & Fish'),
+        ('legumes', 'Legumes'),
+        ('vegetable', 'Vegetable'),
+        ('fruit_fresh', 'Fresh Fruit'),
+        ('fruit_dried', 'Dried Fruit'),
+        ('grain', 'Grains & Cereals'),
+        ('nuts_seeds', 'Nuts & Seeds'),
+        ('dairy', 'Dairy'),
+        ('other', 'Other'),
+    ]
+
     name = models.CharField(max_length=100, unique=True)
     calories_per_100g = models.FloatField()
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='other',
+    )
 
     def save(self, *args, **kwargs):
         self.name = self.name.title()
