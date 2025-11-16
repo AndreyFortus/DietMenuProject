4af6a1032e6a3e57671a20584bab48d09b4c176c
Andrii <andrey020904@gmail.com>
2025-11-16 10:12:55 +0200

    Add product images

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,5 +122,5 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/static/'
