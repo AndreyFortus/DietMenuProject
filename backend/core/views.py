ad2e2a5e132cbec5a4d18a4f6723c763da021da8
Olha Makovlieva <olya.makovleva3002@gmail.com>
2025-11-27 16:25:29 +0200

    add responces for selected products and all products lists

--- backend/core/views.py
+++ backend/core/views.py
@@ -30,9 +30,49 @@ class OptimizeMealAPIView(GenericAPIView):
         if selected_products:
             selected_ids = [p['id'] for p in selected_products]
             products = Dish.objects.filter(id__in=selected_ids)
-        else:
-            products = Dish.objects.all()
 
-        result = optimize_meal(products, P, F, H, E)
+            breakfast_products = products.filter(meal_type='breakfast')
+            lunch_products = products.filter(meal_type='lunch')
+            dinner_products = products.filter(meal_type='dinner')
 
-        return Response(result, status=200)
+            k_b = 0.30
+            k_l = 0.40
+            k_d = 0.30
+
+            P_b, F_b, H_b, E_b = P * k_b, F * k_b, H * k_b, E * k_b
+            P_l, F_l, H_l, E_l = P * k_l, F * k_l, H * k_l, E * k_l
+            P_d, F_d, H_d, E_d = P * k_d, F * k_d, H * k_d, E * k_d
+
+            result_breakfast = optimize_meal(breakfast_products, P_b, F_b, H_b, E_b)
+            result_lunch = optimize_meal(lunch_products, P_l, F_l, H_l, E_l)
+            result_dinner = optimize_meal(dinner_products, P_d, F_d, H_d, E_d)
+
+            return Response({
+                "mode": "selected_products",
+                "breakfast": result_breakfast,
+                "lunch": result_lunch,
+                "dinner": result_dinner
+            }, status=200)
+
+        breakfast_products = Dish.objects.filter(meal_type='breakfast')
+        lunch_products = Dish.objects.filter(meal_type='lunch')
+        dinner_products = Dish.objects.filter(meal_type='dinner')
+
+        k_b = 0.30
+        k_l = 0.40
+        k_d = 0.30
+
+        P_b, F_b, H_b, E_b = P * k_b, F * k_b, H * k_b, E * k_b
+        P_l, F_l, H_l, E_l = P * k_l, F * k_l, H * k_l, E * k_l
+        P_d, F_d, H_d, E_d = P * k_d, F * k_d, H * k_d, E * k_d
+
+        result_breakfast = optimize_meal(breakfast_products, P_b, F_b, H_b, E_b)
+        result_lunch = optimize_meal(lunch_products, P_l, F_l, H_l, E_l)
+        result_dinner = optimize_meal(dinner_products, P_d, F_d, H_d, E_d)
+
+        return Response({
+            'mode': 'full_day',
+            'breakfast': result_breakfast,
+            'lunch': result_lunch,
+            'dinner': result_dinner
+        }, status=200)
