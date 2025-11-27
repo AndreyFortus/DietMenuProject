0521db7591d69cdba016044faf8924a82453fd1f
Olha Makovlieva <olya.makovleva3002@gmail.com>
2025-11-27 16:06:06 +0200

    edit views to match pull

--- backend/core/views.py
+++ backend/core/views.py
@@ -17,10 +17,8 @@ class OptimizeMealAPIView(GenericAPIView):
     def post(self, request):
         data = request.data
 
-        if 'target_macros' in data:
-            data = data['target_macros']
-
-        serializer = self.get_serializer(data=data)
+        macros = data.get('target_macros', {})
+        serializer = self.get_serializer(data=macros)
         serializer.is_valid(raise_exception=True)
 
         P = serializer.validated_data['protein']
@@ -28,7 +26,12 @@ class OptimizeMealAPIView(GenericAPIView):
         H = serializer.validated_data['carbs']
         E = serializer.validated_data['calories']
 
-        products = Dish.objects.all()
+        selected_products = data.get('selected_products', [])
+        if selected_products:
+            selected_ids = [p['id'] for p in selected_products]
+            products = Dish.objects.filter(id__in=selected_ids)
+        else:
+            products = Dish.objects.all()
 
         result = optimize_meal(products, P, F, H, E)
 
