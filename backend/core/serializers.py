e96e0d9e1bd91086322902624f4c2e8da3df3837
Andrii <andrey020904@gmail.com>
2025-11-27 14:29:28 +0200

    аdd meal_type field to Dish for meal classification

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -6,7 +6,7 @@ class DishSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
         fields = ['id', 'type', 'image', 'title', 'description', 'price', 'portion',
-                  'calories', 'protein', 'fat', 'carbs']
+                  'calories', 'protein', 'fat', 'carbs', 'meal_type']
 
 
 class MealOptimizeSerializer(serializers.Serializer):
